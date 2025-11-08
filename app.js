aab602341549ca3bcb00706b9e6483b7ef56ff7c
Lizmary Chardon <lizmary0209@gmail.com>
2025-11-08 04:56:36 -0500

    Fix app.js port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const itemsRouter = require("./routes/clothingitems");
 const { PORT = 3001 } = process.env;
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").catch(() => {});
 
 app.use(express.json());
 
