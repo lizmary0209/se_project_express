10e7ded633ad85d7bf83e07a5752880d84560526
Lizmary Chardon <lizmary0209@gmail.com>
2025-11-23 18:47:17 -0500

    Add: _id for testing

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").catch(() => {});
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  if (!req.user) {
+    req.user = { _id: "5d8b8592978f8bd833ca8133" };
+  }
+  next();
+});
+
 app.use("/users", usersRouter);
 app.use("/items", itemsRouter);
 
