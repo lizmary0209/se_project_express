f6d6612defcdb47c01f1503bf4cdf8ea77e5e186
Lizmary Chardon <lizmary0209@gmail.com>
2025-11-28 17:40:44 -0500

    Fix: eslint error.js

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,4 +1,4 @@
-module.exports = (err, req, res) => {
+module.exports = (err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || "Internal Server Error";
   return res.status(status).json({ message });
