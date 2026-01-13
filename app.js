52ccc378d39fc436b7b0c8515d20db95db076496
Lizmary Chardon <lizmary0209@gmail.com>
2026-01-12 20:54:58 -0500

    Add crash-test route for PM2 recovery check

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(express.json());
 
 app.use(requestLogger);
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use("/", authRouter);
 app.use("/users", usersRouter);
 app.use("/items", itemsRouter);
